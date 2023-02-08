a534c09a52943778cf187ed6426905e8dd9cd61d
Abdullah9431 <123589444+Abdullah9431@users.noreply.github.com>
2023-02-09 00:13:03 +0700

    Add files via upload

--- program01.py
+++ program01.py
@@ -1,3 +1,4 @@
+#(whats the issue in the below code?)
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -116,79 +117,115 @@ NOTE: The test system recognizes recursion ONLY if the recursive
 
 import images
 
-def recursive1(color,image_list):
+
+def recursive1(database, image_list, position, bg_color, count):
+    color = database[position]
+    image1_2 = ()
+    image2_2 = ()
+    image3_2 = ()
+    image4_2 = ()
     image1 = []
     image2 = []
     image3 = []
     image4 = []
     limit_y_axis = len(image_list)
+    # if count == 3:
+    #     print(image_list[0])
     limit_x_axis = len(image_list[0])
     flag_y = True
     for xe in range(limit_y_axis):
-        if image_list[xe][0] == color:
-            for row1 in range(limit_y_axis):
-                flag_x = True
-                if row1==xe:
-                    flag_y = False
-                    continue
-                for pixel1 in range(limit_x_axis):
-                    if image_list[row1][pixel1] == color:
-                        flag_x = False
-                    if flag_y and flag_x:
-                       image4.append(image_list[row1][pixel1])
-                       if 
-                    if flag_y and not flag_x:
-                       image3.append(image_list[row1][pixel1])
-                    if not flag_y and flag_x:
-                       image2.append(image_list[row1][pixel1]) 
-                    if not flag_y and not flag_x:
-                       image1.append(image_list[row1][pixel1])
-                    
-        break
-            
-            
+#issue : in first recursion no value is added
+        try:
+            if image_list[xe][0] == color:
+                for row1 in range(limit_y_axis):
+                    if flag_y:
+                        if image3 != []:
+                            image4_2 = list(image4_2)
+                            image4_2.append(image4)
+                            image4_2 = tuple(image4_2)
+                            image3_2 = list(image3_2)
+                            image3_2.append(image3)
+                            image3_2 = tuple(image3_2)
+                    else:
+                        if image2 != []:
+                            image1_2 = list(image1_2)
+                            image1_2.append(image1)
+                            image1_2 = tuple(image1_2)
+                            image2_2 = list(image2_2)
+                            image2_2.append(image2)
+                            image2_2 = tuple(image2_2)
+                    image1 = []
+                    image2 = []
+                    image3 = []
+                    image4 = []
+                    flag_x = True
+                    if row1 == xe:
+                        flag_y = False
+                        continue
+                    for pixel1 in range(limit_x_axis):
+                        #not passing in first recursion because limit_x_axis = 0
+                        if image_list[row1][pixel1] == color:
+                            flag_x = False
+                            continue
+                        elif flag_y and flag_x:
+                           image4.append(image_list[row1][pixel1])
+                        elif flag_y and not flag_x:
+                           image3.append(image_list[row1][pixel1])
+                        elif not flag_y and flag_x:
+                           image2.append(image_list[row1][pixel1]) 
+                        elif not flag_y and not flag_x:
+                           image1.append(image_list[row1][pixel1])            
+                image1_2 = list(image1_2)
+                image1_2.append(image1)
+                image1_2 = tuple(image1_2)
+                image2_2 = list(image2_2)
+                image2_2.append(image2)
+                image2_2 = tuple(image2_2)
+                break
+        except IndexError:
+            continue
+    list1 = [image1_2, image2_2, image4_2, image3_2]
+    for element in list1:
+        breaker = False
+        for row in element:
+            for pixel in row:
+                if pixel != bg_color:
+                    #print(pixel, '-----------')
+                    count += recursive1(database, element, position + 1, bg_color, count)
+                    breaker = True
+                    break
+            if breaker:
+                break
+        count += 1
+    return count
+
+
 def ex1(input_file, output_file):
-    # write your code here
     image_list = images.load(input_file)
-    #print(image_list)
     bg_color = image_list[0][0]
     database = {}
     temp_database = {}
     count = 0
-    
+    position = 0
     for row in image_list:
-        for curr_pixel,nxt_pixel in zip(row,row[1:]):
+        for curr_pixel, nxt_pixel in zip(row, row[1:]):
             if nxt_pixel != bg_color:
                 if nxt_pixel == curr_pixel:
                     if nxt_pixel not in database:
                         database[nxt_pixel] = 1
                         temp_database[nxt_pixel] = 1
-                        
                     else:
                         temp_database[nxt_pixel] += 1
                         if temp_database[nxt_pixel] > database[nxt_pixel]:
                             database[nxt_pixel] = temp_database[nxt_pixel]
                 else:
                     temp_database[nxt_pixel] = 1
-                # else:
-                #     try:
-                #         if temp_database[nxt_pixel] <= database[nxt_pixel]:
-                #             temp_database[nxt_pixel] = 1
-                #         else:
-                #             database[nxt_pixel] = temp_database[nxt_pixel]
-                #             temp_database[nxt_pixel] = 1
-                #     except:
-                #         continue
-    print(database)
-                        
-            
-        
-        
-        
-    
-    
+    database = sorted(database, reverse=True)
+    return recursive1(database, image_list, position, bg_color, count)
 
 
-if __name__ == '__main__':
-    # write your tests here
-    pass
+ex1('/home/araz/Desktop/HW8rec/puzzles/small01.in.png', 12)
+
+# if __name__ == '__main__':
+#     # write your tests here
+#     ex1('/home/araz/Desktop/HW8rec/puzzles/small01.in.png',12)
